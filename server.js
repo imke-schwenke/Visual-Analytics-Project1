ab167cfe8a81ae7d0515212c1c260aa42119ec6f
Imke Schwenke <imkeschwenke@Air-von-Imke.fritz.box>
2023-04-25 15:24:09 +0200

    server setup

---
+++ server.js
@@ -0,0 +1,43 @@
+const port = 6789
+
+const express = require("express")
+const app = express()
+app.use(express.static("public"))
+const server = app.listen(port)
+
+console.log(`Webserver is running on port ${port}.`)
+
+const socket = require("socket.io")
+const io = socket(server)
+
+io.sockets.on("connection", (socket) => {
+console.log(`Client ${socket.id} connected.`)
+
+let disconnect = () => {
+console.log(`Client ${socket.id} disconnected.`)
+}
+
+// let get_example_data = (parameters) => {
+//     console.log('Received data request with these parameters: ${parameters}')
+
+//     socket.emit("example_data", { hello: "world" })
+// }
+
+let get_example_data = (parameters) => {
+    console.log('Received data request with these parameters: ${parameters}')
+    fs.readFile("./data/testdata.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        let json_data = JSON.parse(data)
+        socket.emit("example_data", json_data)
+    })
+}
+
+socket.on("disconnect", disconnect)
+
+socket.on("get_example_data", get_example_data)
+})
+
+const fs = require("fs")
