eb63bc284ab102fcb296a6fd55b0b5803ee36b27
Imke Schwenke <imkeschwenke@MacBook-Air-von-Imke.local>
2023-05-04 15:05:06 +0200

    changed first vis

--- public/app.js
+++ public/app.js
@@ -18,41 +18,74 @@
  		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	//add Title, Subtitle and Axis Labels for Visualization
-	addTitle(svg, "Rating of Games with a Long Playtime over the Years");
+	addTitle(svg, "Amount of published Games over the Years");
 	addSubtitle(svg);
-	addAxisTitle(svg, "Years", "Average Rating") 
+	addAxisTitle(svg, "Years", "Amount") 
 
  	//Read the data
  	d3.json("/boardgames_40.json").then(function(data) {
  		data = processBoardGameData(data);
-		data = temporalProcessing(data);
+		data_long = longTemporalProcessing(data);
+		data_medium = mediumTemporalProcessing(data);
+		data_short = shortTemporalProcessing(data);
 
  		// scale x values
- 		const x = ScaleX(data);
+ 		const x = ScaleX(data_long);
 
  		// create and append x Axis
- 		getXAxis(svg, data);
+ 		getXAxis(svg, data_long);
 
  		// scale y values
- 		const y = ScaleY(data);
+ 		const y = ScaleY(data_long);
 
  		// create and append x Axis
- 		getYAxis(svg, data);
+ 		getYAxis(svg, data_long);
+
+		getLegend(svg, ['Short Games','Medium Games','Long Games'], ['green','red','steelblue']);
 
  		// Add the line
  		svg.append("path")
- 			.datum(data)
+ 			.datum(data_short)
  			.attr("fill", "none")
- 			.attr("stroke", "steelblue")
+ 			.attr("stroke", "green")
  			.attr("stroke-width", 1.5)
  			.attr("d", d3.line()
  				.x(function(d) {
  					return x(d.year)
  				})
  				.y(function(d) {
- 					return y(d.rating)
+ 					return y(d.amount)
  				})
  			)
+		// Add the line
+		svg.append("path")
+		.datum(data_medium)
+		.attr("fill", "none")
+		.attr("stroke", "red")
+		.attr("stroke-width", 1.5)
+		.attr("d", d3.line()
+			.x(function(d) {
+				return x(d.year)
+			})
+			.y(function(d) {
+				return y(d.amount)
+			})
+		)
+
+		// Add the line
+		svg.append("path")
+		.datum(data_long)
+		.attr("fill", "none")
+		.attr("stroke", "steelblue")
+		.attr("stroke-width", 1.5)
+		.attr("d", d3.line()
+			.x(function(d) {
+				return x(d.year)
+			})
+			.y(function(d) {
+				return y(d.amount)
+			})
+		)
  	})
  }
 
@@ -69,7 +102,7 @@
  function ScaleY(data) {
  	var y = d3.scaleLinear()
  		.domain([0, d3.max(data, function(d) {
- 			return +d.rating
+ 			return +d.amount
  		})])
  		.range([height + 380, 0]);
  	return y;
@@ -110,6 +143,14 @@
     .attr("dy", ".75em")
     .attr("transform", "rotate(-90)")
     .text(yLabel);
+}
+
+// function for appending legend
+function getLegend(svg, textArray, colorArray){
+	for (let i = 0; i< textArray.length; i++){
+		svg.append("circle").attr("cx",2*width+100).attr("cy",130+i*30).attr("r", 6).style("fill", colorArray[i])
+		svg.append("text").attr("x", 2*width+120).attr("y", 130+ i*30).text(textArray[i]).style("font-size", "15px").attr("alignment-baseline","middle")
+	}	
 }
 
 // function for adding a title for the visualization in the svg
@@ -239,13 +280,52 @@ function addSubtitle(svg){
  	return years;
  }
 
- // function which continues the data processing for the line graph
- function temporalProcessing(data){
+ // function which continues the data processing for the line graph for long games
+ function longTemporalProcessing(data){
 	result=[];
+	amount = 0;
 	for (let i = 0; i < Object.keys(data).length; i++) {
-		if(Object.keys(Object.values(data)[i]) == 'long'){
-			let temp = Object.values(data)[i];
-			result.push({year:Object.keys(data)[i], rating: Object.values(temp)[0]['avg_rating']})
+		for(let j = 0; j < Object.keys(Object.values(data)[i]).length; j++){
+			if(Object.keys(Object.values(data)[i])[j] == 'long'){
+				let temp = Object.values(data)[i]['long'];
+				amount += temp['amountOfGames'];
+				result.push({year:Object.keys(data)[i], amount: amount})
+			}
+
+		}
+	}
+	return result
+ }
+
+// function which continues the data processing for the line graph for long games
+function mediumTemporalProcessing(data){
+	result=[];
+	amount = 0;
+	for (let i = 0; i < Object.keys(data).length; i++) {
+		for(let j = 0; j < Object.keys(Object.values(data)[i]).length; j++){
+			if(Object.keys(Object.values(data)[i])[j] == 'medium'){
+				let temp = Object.values(data)[i]['medium'];
+				amount += temp['amountOfGames'];
+				result.push({year:Object.keys(data)[i], amount: amount})
+			}
+
+		}
+	}
+	return result
+ }
+
+// function which continues the data processing for the line graph for short games
+function shortTemporalProcessing(data){
+	result=[];
+	amount = 0;
+	for (let i = 0; i < Object.keys(data).length; i++) {
+		for(let j = 0; j < Object.keys(Object.values(data)[i]).length; j++){
+			if(Object.keys(Object.values(data)[i])[j] == 'short'){
+				let temp = Object.values(data)[i]['short'];
+				amount += temp['amountOfGames'];
+				result.push({year:Object.keys(data)[i], amount: amount})
+			}
+
 		}
 	}
 	return result
